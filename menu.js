8b48cd1816f9a0dac99145487266190384f33cfe
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2024-09-16 15:47:14 -0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+const mycanvas2 = document.getElementById('myCanvas2');
+const ctx2 = mycanvas2.getContext('2d');
+
+ctx2.fillStyle = 'rgb(18, 13, 118)';
+ctx2.fillRect(0, 0, mycanvas2.width, mycanvas2.height);
+
