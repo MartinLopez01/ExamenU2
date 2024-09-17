8f2115b12e126ee14d018bfd8b56c30c1630cd6d
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2024-09-17 04:22:29 -0700

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,17 @@
+
 const mycanvas2 = document.getElementById('myCanvas2');
 const ctx2 = mycanvas2.getContext('2d');
 
 ctx2.fillStyle = 'rgb(18, 13, 118)';
 ctx2.fillRect(0, 0, mycanvas2.width, mycanvas2.height);
 
+ctx2.fillStyle = "white";
+ctx2.font = "20px Arial";
+ctx2.fillText("Puntuación: ", 10, 30); 
+
+ctx2.fillText("Arriba: W " , 10, 200); 
+ctx2.fillText("Abajo: S " , 10, 230); 
+ctx2.fillText("Izquierda: A " , 10, 260); 
+ctx2.fillText("Derecha: D" , 10, 290); 
+ctx2.fillText("Poner bomba: ESPACIO" , 10, 320); 
+ctx2.fillText("NO HAY PAUSA XDD" , 10, 350); 
