1546dbfc6335d5989c07eabab14a1f427821da61
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2024-09-16 21:21:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const ctx = mycanvas.getContext('2d');
 
 let bombas = []
 let obstaculos = []
-let walls = []
+let pared = []
 
 const tamano = 40;
 let x = 0;
 let y = 0;
-let velocidad = 40;
+let velocidad = tamano;
 let direccion = "";
 
 const filas = mycanvas.height / tamano;
@@ -17,6 +17,8 @@ const fondo = new Image();
 fondo.src = "fondo.jpg";
 const jugadorImg = new Image();
 jugadorImg.src = "Bob.webp";
+const paredImg = new Image();
+paredImg.src = "pared.webp";
 
 class Rectangulo {
     constructor(x, y, w, h, c) {
@@ -37,6 +39,52 @@ class Rectangulo {
 
 let jugador = new Rectangulo(x, y, tamano, tamano, jugadorImg);
 
+function paredes() {
+
+    const wallSpacing = tamano * 3; 
+    const wallSize = tamano;
+
+    for (let x = 0; x < mycanvas.width; x += wallSpacing) {
+        for (let y = 0; y < mycanvas.height; y += wallSpacing) {
+           
+            if (!(x === jugador.x && y === jugador.y)) {
+                pared.push(new Rectangulo(x, y, wallSize, wallSize,paredImg));
+            }
+        }
+    }
+
+    obstaculos = obstaculosAleatorios(350, true);
+}
+
+function obstaculosAleatorios(contador, posJugador) {
+    let posicion = [];
+    while (posicion.length < contador) {
+        let x = Math.floor(Math.random() * columnas) * tamano;
+        let y = Math.floor(Math.random() * filas) * tamano;
+
+        if (posJugador && (x === jugador.x && y === jugador.y || posPared(x, y))) {
+            continue;
+        }
+
+        if (!posicion.some(pos => pos.x === x && pos.y === y)) {
+            posicion.push({ x, y, size: tamano });
+        }
+    }
+    return posicion;
+}
+
+function posPared(x, y) {
+    for (let i = 0; i < pared.length; i++) {
+        if (x === pared[i].x && y === pared[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
 document.addEventListener("keydown", function (e) {
     switch (e.keyCode) {
         case 87:
@@ -44,28 +92,28 @@ document.addEventListener("keydown", function (e) {
             if (jugador.y <= 0) {
                 jugador.y = mycanvas.height;
             }
-            direccion = ""; 
+            direccion = "";
             break;
         case 83:
             jugador.y += velocidad;
             if (jugador.y >= mycanvas.height) {
                 jugador.y = 0;
             }
-            direccion = ""; 
+            direccion = "";
             break;
         case 65:
             jugador.x -= velocidad;
             if (jugador.x <= 0) {
                 jugador.x = mycanvas.width;
             }
-            direccion = ""; 
+            direccion = "";
             break;
         case 68:
             jugador.x += velocidad;
             if (jugador.x >= mycanvas.width) {
                 jugador.x = 0;
             }
-            direccion = ""; 
+            direccion = "";
             break;
         case 32:
             // ponerBomba()
@@ -76,11 +124,21 @@ document.addEventListener("keydown", function (e) {
 function pintar() {
     ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
     ctx.drawImage(fondo, 0, 0, mycanvas.width, mycanvas.height);
+   
+    pared.forEach(wall => {
+        ctx.drawImage(wall.c, wall.x, wall.y, wall.w, wall.h);
+    });
+
+    obstaculos.forEach(obstaculo => {
+        ctx.fillStyle = "black"; 
+        ctx.fillRect(obstaculo.x, obstaculo.y, obstaculo.size, obstaculo.size);
+    });
+
     ctx.drawImage(jugador.c, jugador.x, jugador.y, jugador.w, jugador.h);
 
-    
+
 
     requestAnimationFrame(pintar);
 }
-
-requestAnimationFrame(pintar);
+paredes();
+pintar();
