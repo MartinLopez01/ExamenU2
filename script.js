0a8e016a3e8ce297ef791e702627d38b884f4c83
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2024-09-17 02:47:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const mycanvas = document.getElementById('myCanvas');
 const ctx = mycanvas.getContext('2d');
 
-let bombas = []
-let obstaculos = []
-let pared = []
+let bombas = [];
+let obstaculos = [];
+let pared = [];
 
 const tamano = 40;
 let x = 0;
@@ -40,15 +40,13 @@ class Rectangulo {
 let jugador = new Rectangulo(x, y, tamano, tamano, jugadorImg);
 
 function paredes() {
+    const espacioPared = tamano * 3;
+    const tamanoPared = tamano;
 
-    const wallSpacing = tamano * 3; 
-    const wallSize = tamano;
-
-    for (let x = 0; x < mycanvas.width; x += wallSpacing) {
-        for (let y = 0; y < mycanvas.height; y += wallSpacing) {
-           
+    for (let x = 0; x < mycanvas.width; x += espacioPared) {
+        for (let y = 0; y < mycanvas.height; y += espacioPared) {
             if (!(x === jugador.x && y === jugador.y)) {
-                pared.push(new Rectangulo(x, y, wallSize, wallSize,paredImg));
+                pared.push(new Rectangulo(x, y, tamanoPared, tamanoPared, paredImg));
             }
         }
     }
@@ -82,41 +80,114 @@ function posPared(x, y) {
     return false;
 }
 
+function ponerBomba() {
+    bombas.push({
+        x: jugador.x,
+        y: jugador.y,
+        timer: 3
+    });
+}
 
 
+function explotarBomba(ponerUnaBomba) {
+    let bomba = bombas[ponerUnaBomba];
+
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(bomba.x - tamano, bomba.y, tamano * 3, tamano);
+
+
+    ctx.fillRect(bomba.x, bomba.y - tamano, tamano, tamano * 3);
+
+
+    obstaculos = obstaculos.filter(obstaculo => {
+        if (verificarColision(obstaculo, bomba)) {
+            return false;
+        }
+        return true;
+    });
+
+    bombas.splice(ponerUnaBomba, 1);
+}
+
+
+function verificarColision(obstaculo, bomba) {
+
+    let rangoExplosivo = {
+        xMin: bomba.x - tamano,
+        xMax: bomba.x + tamano * 2,
+        yMin: bomba.y - tamano,
+        yMax: bomba.y + tamano * 2
+    };
+
+
+    return (
+        obstaculo.x < rangoExplosivo.xMax &&
+        obstaculo.x + obstaculo.size > rangoExplosivo.xMin &&
+        obstaculo.y < rangoExplosivo.yMax &&
+        obstaculo.y + obstaculo.size > rangoExplosivo.yMin
+    );
+}
+
+function puedeMover(x, y) {
+    let jugadorTemp = new Rectangulo(x, y, jugador.w, jugador.h, jugador.c);
+
+
+    for (let i = 0; i < pared.length; i++) {
+        if (jugadorTemp.seTocan(pared[i])) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < obstaculos.length; i++) {
+        if (jugadorTemp.seTocan(new Rectangulo(obstaculos[i].x, obstaculos[i].y, tamano, tamano))) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 document.addEventListener("keydown", function (e) {
     switch (e.keyCode) {
         case 87:
-            jugador.y -= velocidad;
+            if (puedeMover(jugador.x, jugador.y - velocidad)) {
+                jugador.y -= velocidad;
+            }
             if (jugador.y <= 0) {
                 jugador.y = mycanvas.height;
             }
             direccion = "";
             break;
         case 83:
-            jugador.y += velocidad;
+            if (puedeMover(jugador.x, jugador.y + velocidad)) {
+                jugador.y += velocidad;
+            }
             if (jugador.y >= mycanvas.height) {
                 jugador.y = 0;
             }
             direccion = "";
             break;
         case 65:
-            jugador.x -= velocidad;
+            if (puedeMover(jugador.x - velocidad, jugador.y)) {
+                jugador.x -= velocidad;
+            }
             if (jugador.x <= 0) {
                 jugador.x = mycanvas.width;
             }
             direccion = "";
             break;
         case 68:
-            jugador.x += velocidad;
+            if (puedeMover(jugador.x + velocidad, jugador.y)) {
+                jugador.x += velocidad;
+            }
             if (jugador.x >= mycanvas.width) {
                 jugador.x = 0;
             }
             direccion = "";
             break;
         case 32:
-            // ponerBomba()
+            ponerBomba();
             break;
     }
 });
@@ -124,21 +195,34 @@ document.addEventListener("keydown", function (e) {
 function pintar() {
     ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
     ctx.drawImage(fondo, 0, 0, mycanvas.width, mycanvas.height);
-   
+
+
     pared.forEach(pared => {
         ctx.drawImage(pared.c, pared.x, pared.y, pared.w, pared.h);
     });
 
+
     obstaculos.forEach(obstaculo => {
-        ctx.fillStyle = "black"; 
+        ctx.fillStyle = "black";
         ctx.fillRect(obstaculo.x, obstaculo.y, obstaculo.size, obstaculo.size);
+    });
+
+
+    bombas.forEach(bomba => {
+        ctx.fillStyle = "red";
+        ctx.fillRect(bomba.x, bomba.y, tamano, tamano);
+
+
+        bomba.timer -= 0.05;
+        if (bomba.timer <= 0) {
+            explotarBomba(bombas.indexOf(bomba));
+        }
     });
 
     ctx.drawImage(jugador.c, jugador.x, jugador.y, jugador.w, jugador.h);
 
-
-
     requestAnimationFrame(pintar);
 }
+
 paredes();
 pintar();
